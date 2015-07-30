2c6beb2aba3013a4f77a50a467f825f2f053a553
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2015-07-30 13:41:23 +0300

    Disable hot reloading in test environment

--- build/config.js
+++ build/config.js
@@ -6,9 +6,10 @@ const src = path.join(root, 'src')
 const dist = path.join(root, 'dist')
 
 const prod = process.env.NODE_ENV === 'production'
-const dev = !prod
+const test = process.env.NODE_ENV === 'test'
+const dev = !prod && !test
 
-const hotLoader = process.env.HOT_RELOAD === 'react-hot-loader'
+const hotLoader = !test && process.env.HOT_RELOAD === 'react-hot-loader'
 
 const nodeModulesDirectory = path.join(root, 'node_modules')
 const nodeModulesExternals = fs.readdirSync(nodeModulesDirectory)
