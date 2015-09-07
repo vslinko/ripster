193d1c627ea6fc3247f9747e5cd54b1e1382d82e
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2015-09-08 00:06:05 +0300

    bin/create_bundle

---
+++ scripts/cmd/createBundle.js
@@ -0,0 +1,28 @@
+import {existsAsync, mkdirAsync} from 'fs-extra-promise';
+import {join} from 'path';
+import {
+  mainAnswers,
+  bundlesDir
+} from '../utils';
+
+const questions = [
+  {
+    type: 'input',
+    name: 'bundleName',
+    message: 'Bundle name',
+    async validate(name) {
+      if (await existsAsync(join(bundlesDir, name))) {
+        return `Bundle "${name}" already exists`;
+      }
+
+      return true;
+    },
+  },
+];
+
+mainAnswers(questions, async (context) => {
+  const directory = join(bundlesDir, context.bundleName);
+
+  await mkdirAsync(directory);
+  await mkdirAsync(join(directory, 'components'));
+});
