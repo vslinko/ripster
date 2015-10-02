1043c43b29521fbe6d06a2531998845ef9d5efe3
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2015-10-02 14:09:38 +0300

    redux-logger

--- src/frontend/createStore.js
+++ src/frontend/createStore.js
@@ -11,8 +11,10 @@ export default function createAppStore(initialState) {
 
   if (process.env.NODE_ENV !== 'production') {
     const {devTools, persistState} = require('redux-devtools');
+    const logger = require('redux-logger');
 
     finalCreateStore = compose(
+      applyMiddleware(logger()),
       devTools(),
       persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
     )(finalCreateStore);
